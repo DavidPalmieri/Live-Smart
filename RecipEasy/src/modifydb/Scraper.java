dcc2b2825577bce82c46d0d58d01c616bacf3def
cjcosta <cjcosta@my.waketech.edu>
2016-03-02 03:05:59 -0500

    Scraper Class Update
    General progress update.  Next update will make changes to Recipe object
    properties and methods to better suit Scraper classes data retrieval.

--- RecipEasy/src/modifydb/Scraper.java
+++ RecipEasy/src/modifydb/Scraper.java
@@ -33,10 +33,10 @@ public class Scraper
 		//An ArrayList of test recipe URLs
 		ArrayList<String> urls = new ArrayList<String>();		
 		urls.add("http://www.bettycrocker.com/recipes/make-ahead-cheeseburger-lasagna/122c69cd-e318-406f-b5e7-67d93d899537");
-		//urls.add("http://www.bettycrocker.com/recipes/sweet-potato-coconut-and-gingerroot-soup/37e846bc-53cf-4615-9377-d2456511da5c");
-		//urls.add("http://www.bettycrocker.com/recipes/tropical-smoothie-bowls/8e5666f0-6796-4db1-9e56-022760a97d8c");
-		//urls.add("http://www.bettycrocker.com/recipes/gluten-free-best-ever-banana-bread/85ebf86a-972e-4768-b759-32191f5e8a4f");
-		//urls.add("http://www.bettycrocker.com/recipes/teenage-mutant-ninja-turtles-cupcakes/007bfa56-e876-4c26-ac3e-985a6b5ea466");
+		urls.add("http://www.bettycrocker.com/recipes/sweet-potato-coconut-and-gingerroot-soup/37e846bc-53cf-4615-9377-d2456511da5c");
+		urls.add("http://www.bettycrocker.com/recipes/tropical-smoothie-bowls/8e5666f0-6796-4db1-9e56-022760a97d8c");
+		urls.add("http://www.bettycrocker.com/recipes/gluten-free-best-ever-banana-bread/85ebf86a-972e-4768-b759-32191f5e8a4f");
+		urls.add("http://www.bettycrocker.com/recipes/teenage-mutant-ninja-turtles-cupcakes/007bfa56-e876-4c26-ac3e-985a6b5ea466");
 				
 		//An ArrayList of recipes used for testing this class
 		ArrayList<Recipe> recipes = new ArrayList<Recipe>();
@@ -62,21 +62,40 @@ public class Scraper
 	            e.printStackTrace();
 	        }
 			
+			//Get the set of html Elements that will be searched for data
+			Elements elements = html.getAllElements();
+			
 			//Get the title of the recipe from the html
-			String title = head(html);
+			String title = title(html);
+			System.out.println(title);
 			
 			//Get the summary of the recipe
-			String summary = summary(html);
+			String summary = summary(elements);
+			System.out.println(summary);
+			
+			//Get the picture address (and later dl the image nd save address)
+			String picture = picture(elements);
+			System.out.println(picture);
 			
 			//Get the prep time, total time, and servings
-			String[] timeServings = timeServings(html);
+			String[] timeServings = timeServings(elements);
+			System.out.println("Prep Time: " + timeServings[0]);
+			System.out.println("Total time: " + timeServings[1]);
+			System.out.println("Servings: " + timeServings[2]);
+			
+			//Get all of the nutrition information
+			String[] nutrition = nutrition(elements);
+			
+			//Get the copyright information
+			String trademark = trademark(elements);
+			System.out.println(trademark);
+			
 			
 			//Create the Recipe object with the information found and add to the ArrayList
 			Recipe recipe = new Recipe(url, title);
 			recipes.add(recipe);
 			
-			//Output the data to check for consistency
-			System.out.println(recipes.get(i).getDetails()[0]);
+			System.out.println();
 		}
 		
 		//Send data to be serialized for use in the user interface
@@ -84,7 +103,7 @@ public class Scraper
 	}
 	
 	//Parse the html to get the title as a String
-	private static String head(Document html)
+	private static String title(Document html)
 	{
 		String head = html.head().text();
 		String[] trimmed = head.split(" recipe from Betty Crocker");
@@ -92,9 +111,8 @@ public class Scraper
 	}
 	
 	//Parse the html to get the summary as a String
-	private static String summary(Document html)
+	private static String summary(Elements e)
 	{
-		Elements e = html.getAllElements();
 		for (int i = 0; i < e.size(); i++)
 		{
 			if(e.get(i).className().equalsIgnoreCase("recipePartDescriptionText"))
@@ -106,68 +124,93 @@ public class Scraper
 	}
 	
 	//Parse the html to get the prep time, total time, and total servings
-	private static String[] timeServings(Document html)
+	private static String[] timeServings(Elements e)
 	{
 		String[] output = new String[3];
 		
-		Elements e = html.getAllElements();
-		Elements f = new Elements();
+		//prep time: find the element, parse the content, convert to our time format
+		output[0] = convertTime(parseContent(tagSearch(e, "meta", "prepTime")));
 		
-		for (int i = 0; i < e.size(); i++)
-		{
-			if (e.get(i).tag().toString().equalsIgnoreCase("meta"))
-			{
-				f.add(e.get(i));
-			}
-		}
+		//total time: find the element, parse the content, convert to our time format
+		output[1] = convertTime(parseContent(tagSearch(e, "meta", "totalTime")));
 		
-		for (int i = 0; i < f.size(); i++)
-		{
-			if (f.get(i).toString().contains("prepTime"))
-			{
-				output[0] = convertTime(parseContents(f.get(i).toString()));
-			}
-			
-			if (f.get(i).toString().contains("totalTime"))
-			{
-				output[1] = convertTime(parseContents(f.get(i).toString()));
-			}
-			
-			if (f.get(i).toString().contains("recipeYield"))
-			{
-				output[2] = parseContents(f.get(i).toString());
-			}
-		}
+		//servings: find the element, parse the content
+		output[2] = parseContent(tagSearch(e, "meta", "recipeYield"));
 		
 		return output;
 	}
 	
 	//Parse the html to get the trademark information
-	private static String trademark(Document html)
+	private static String trademark(Elements e)
 	{
-		return null;
-	}
-	
-	//Parse the html to get the serving size
-	private static String servingSize(Document html)
-	{
+		for (int i = 0; i < e.size(); i++)
+		{
+			if(e.get(i).className().equalsIgnoreCase("recipePartCopyRightText"))
+			{
+				return e.get(i).text();
+			}
+		}
 		return null;
 	}
 	
 	//Parse the html for all of the nutrition information
-	private static Integer[] nutrition(Document html)
+	private static String[] nutrition(Elements e)
 	{
-		return null;
+		String[] output = new String[16];
+		//serving size, calories, calFat, totFat, satFat, transFat, cholesterol,
+		//sodium, carbs, fiber, sugar, protein, vitA, vitC, calcium, iron
+		output[0] = parseContent(tagSearch(e, "meta", "servingSize"));
+		output[1] = parseContent(tagSearch(e, "meta", "calories"));
+		output[2] = parseContent(tagSearch(e, "meta", ""));
+		output[3] = parseContent(tagSearch(e, "meta", ""));
+		output[4] = parseContent(tagSearch(e, "meta", "saturatedFatContent"));
+		output[5] = parseContent(tagSearch(e, "meta", "transFatContent"));
+		output[6] = parseContent(tagSearch(e, "meta", "cholesterolContent"));
+		output[7] = parseContent(tagSearch(e, "meta", "sodiumContent"));
+		output[8] = parseContent(tagSearch(e, "meta", "carbohydrateContent"));
+		output[9] = parseContent(tagSearch(e, "meta", "fiberContent"));
+		output[10] = parseContent(tagSearch(e, "meta", "sugarContent"));
+		output[11] = parseContent(tagSearch(e, "meta", "proteinContent"));
+		output[12] = parseContent(tagSearch(e, "meta", ""));
+		output[13] = parseContent(tagSearch(e, "meta", ""));
+		output[14] = parseContent(tagSearch(e, "meta", ""));
+		output[14] = parseContent(tagSearch(e, "meta", ""));
+				
+		return output;
 	}
 	
 	//Parse the html to find the address of the picture and download it to the computer
-	private static String picture(Document html)
+	private static String picture(Elements e)
 	{
-		return null;
+		return parseContent(tagSearch(e, "meta", "image"));
+	}
+	
+	//Search through the html for a specific tag type and tag contents
+	private static String tagSearch(Elements e, String tag, String contents)
+	{
+		Elements matchElements = new Elements();
+		
+		for (int i = 0; i < e.size(); i++)
+		{
+			if (e.get(i).tag().toString().equalsIgnoreCase(tag))
+			{
+				matchElements.add(e.get(i));
+			}
+		}
+		
+		for (int i = 0; i < matchElements.size(); i++)
+		{
+			if (matchElements.get(i).toString().contains(contents))
+			{
+				return matchElements.get(i).toString();
+			}
+		}
+		
+		return null;	
 	}
 	
 	//Parse the contents text out of a tag by splitting the input String into 3 pieces
-	private static String parseContents(String input)
+	private static String parseContent(String input)
 	{
 		String[] piece = input.split("content=\"");
 		piece = piece[1].split("\"");
@@ -187,11 +230,11 @@ public class Scraper
 			
 			if (hours > 1)
 			{
-				output += " Hours and ";
+				output += " Hours ";
 			}
 			else
 			{
-				output += " Hour and ";
+				output += " Hour ";
 			}
 		}
 		
@@ -200,6 +243,10 @@ public class Scraper
 		
 		if (min != 0)
 		{
+			if(hours != 0 )
+			{
+				output += "and ";
+			}
 			output +=  + min;
 			
 			if (min > 1)
