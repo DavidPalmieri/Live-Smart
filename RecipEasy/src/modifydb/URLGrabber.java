2ef0b3ade91c426736f798ef9e6d51947e6d8eb1
cjcosta <cjcosta@my.waketech.edu>
2016-03-03 04:45:18 -0500

    URLGrabber Class
    commit part two

---
+++ RecipEasy/src/modifydb/URLGrabber.java
@@ -0,0 +1,48 @@
+package modifydb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class URLGrabber
+{
+	
+	String fileName;
+
+	public URLGrabber(String fileName)
+	{
+		this.fileName = fileName;
+	}
+	
+	public String getCategory()
+	{
+		String[] category = fileName.split(".txt");
+		return category[0];
+	}
+	
+	public ArrayList<String> getLinks() throws IOException 
+	{
+		ArrayList<String> links = new ArrayList<String>();
+		
+		InputStream in =  getClass().getResourceAsStream("/modifydb/CategoryLinks/" + fileName);
+		BufferedReader br = new BufferedReader( new InputStreamReader(in, "utf-8"));
+	 
+		String line = null;
+		while ((line = br.readLine()) != null) 
+		{
+			if (line.charAt(line.length() - 13) == '-' && line.charAt(line.length() - 18) == '-' && 
+					line.charAt(line.length() - 23) == '-' && line.charAt(line.length() - 28) == '-' && 
+					line.charAt(line.length() - 37) == '/')
+			{
+				links.add(line);
+			}
+		}
+	 
+		br.close();
+		
+		return links;
+	}
+
+}
