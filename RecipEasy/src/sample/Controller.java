b7e464ac1d1745ae49d2bb9e19db6011514609ba
bsadams1 <bsadams1@my.waketech.edu>
2016-04-03 18:52:18 -0400

    FX sample added

---
+++ RecipEasy/src/sample/Controller.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Controller {
+
+    public void loginButtonClicked(){
+        System.out.println("User logged in...");
+    }
+
+    public void settingsButtonClicked(){
+        System.out.println("Going to Settings Menu...");
+    }
+
+}
