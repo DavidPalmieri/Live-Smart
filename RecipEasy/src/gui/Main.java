308af58975c8a317f9c820f637ea29bd77244340
bsadams1 <bsadams1@my.waketech.edu>
2016-04-10 10:32:14 -0400

    Cleanup Main and Remove Old Interface

--- RecipEasy/src/gui/Main.java
+++ RecipEasy/src/gui/Main.java
@@ -2,14 +2,10 @@ package gui;
 
 import data.Recipe;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,32 +15,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        primaryStage.setTitle("RecipEasy");
-        
-        //name column
-        TableColumn<Recipe, String> nameColumn = new TableColumn<>("Name");
-        nameColumn.setMinWidth(200);
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
-        
-        //table
-        table = new TableView<>();
-        table.setItems(getRecipe());
-        table.getColumns().addAll(nameColumn);
-        
+        primaryStage.setTitle("RecipEasy");        
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-    }
+    } 
     
-    //Get all of the recipes
-    public ObservableList<Recipe> getRecipe(){
-		ObservableList<Recipe> recipes = FXCollections.observableArrayList();
-		recipes.add(new Recipe("Hamburger"));
-		recipes.add(new Recipe("Pizza"));
-		recipes.add(new Recipe("Ice Cream"));
-    	return recipes;    	
-    }
 }
