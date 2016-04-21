048f746d0ebccb3153f8643b9aa35fe3912f3c6f
cjcosta <cjcosta@my.waketech.edu>
2016-04-20 23:14:57 -0400

    Conflict resolution

---
+++ RecipEasy/src/login/loginController.java
@@ -0,0 +1,82 @@
+
+
+package login;
+ 
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+import data.DatabaseInterface.DBUsersIntf;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+ 
+public class loginController {
+    @FXML private Text actiontarget;
+    @FXML TextField user;
+	@FXML PasswordField pw;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        
+        
+        System.out.printf("%s logged in...\n", user.getText());  	//user: test
+        System.out.printf("password %s\n", pw.getText());			//password: password
+        
+        //Get the fields input by the user
+        String username = user.getText();
+        String password = pw.getText();
+        
+        //Open a connection to the DB and get the encrypted password for the given username
+        DBUsersIntf dbLookup = new DBUsersIntf();
+        String encryptedPassword = dbLookup.getPassword(username);
+        dbLookup.close();
+        
+        //Use Jasypt's password encryptor to verify whether the plain-text and encrypted passwords match
+        if (!encryptedPassword.equalsIgnoreCase("")){
+        	StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
+    		if (passwordEncryptor.checkPassword(password, encryptedPassword)) {
+    			//success, the input password matches the password on file
+    			System.out.println("Successful password match\n");
+    			try {
+    		        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("homePage.fxml"));
+    		                Parent root1 = (Parent) fxmlLoader.load();
+    		                Stage stage = new Stage();
+    		                stage.setScene(new Scene(root1));  
+    		                stage.show();
+    		        } catch(Exception e) {
+    		           e.printStackTrace();
+    		          }
+    		}
+    		else {
+    			//fail, the passwords do not match
+    			System.out.println("invalid password\n");
+    			actiontarget.setText("invalid password");
+    		}	
+        } else { // empty password string = username not found in Users table
+        	System.out.println("username not found\n");
+        	actiontarget.setText("username not found");
+        }
+    }
+    
+@FXML protected void handleRegistrationButtonAction(ActionEvent event) {
+	 try {
+	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml_register.fxml"));
+	                Parent root1 = (Parent) fxmlLoader.load();
+	                Stage stage = new Stage();
+	                stage.setTitle("RecipEasy Registration");
+	                stage.setScene(new Scene(root1,300,300));  
+	                stage.show();
+	        } catch(Exception e) {
+	           e.printStackTrace();
+	          }
+        
+        
+	}
+}
