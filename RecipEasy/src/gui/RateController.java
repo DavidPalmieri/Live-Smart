0d4f2faea0abe4e033ef1f740e02d97ce523b80e
dpalmieri1 <dpalmieri@my.waketech.edu>
2016-04-22 15:08:36 -0400

    Fix pictures and more

---
+++ RecipEasy/src/gui/RateController.java
@@ -0,0 +1,49 @@
+
+
+package gui;
+ 
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
+import data.DatabaseInterface.DBUsersIntf;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+ 
+public class RateController {
+    @FXML private Text actiontarget;
+    @FXML TextField satRate;
+    @FXML TextField costRate;
+    @FXML TextField easeRate;
+	@FXML private GridPane gp;
+	
+	DataInterface di = new DataInterface();
+    
+    @FXML protected void handleCloseButtonAction(ActionEvent event) {
+        
+    	try {
+            Stage current = (Stage) gp.getScene().getWindow();
+            current.close();
+            
+    } catch(Exception e) {
+       e.printStackTrace();
+      }
+    }
+@FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        
+        //Get the fields input by the user
+        int satR = Integer.parseInt(satRate.getText());
+        int costR = Integer.parseInt(costRate.getText());
+        int easeR = Integer.parseInt(easeRate.getText());
+        
+        
+        System.out.printf("%d\n%d\n%d\n", satR, costR, easeR);
+    }
+    
+}
--- RecipEasy/src/gui/RecipeController.java
+++ RecipEasy/src/gui/RecipeController.java
@@ -12,11 +12,17 @@ import data.Recipes.BasicInfo;
 import data.Recipes.Recipe;
 import data.Users.Rating;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
 public class RecipeController {	
 	
+	//give the controller access to graphic controls
 	@FXML Label lTitle;
 	@FXML Label lCatVal;
 	@FXML Label lRateVal;
@@ -27,8 +33,42 @@ public class RecipeController {
 	@FXML ImageView imgPic;
 	@FXML Button butRate;
 	@FXML Button butBack;
+	
+	@FXML private AnchorPane aPane;
+	
+	
+	//pop up the rating page to allow for rating recipes
+	public void ratingButClick(){
+		try {
+    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RatingPage.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+//            Controller controller = fxmlLoader.<Controller>getController();
+//            controller.setUser(userID, username);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));  
+            stage.show();
+		
+            
+    } 
+		catch(Exception e) {
+       e.printStackTrace();
+      }
+	} 
+	
+	// closes the window
+	public void closeButClick(){
+		try {
+            Stage current = (Stage) aPane.getScene().getWindow();
+            current.close();
+            
+    } catch(Exception e) {
+       e.printStackTrace();
+      }
+	} 
+	
+	
 
-   
+   //generate a random recipe and display the info for testing.
     public void randomButtonClicked(){
     	System.out.println("Displaying random recipe...");      
 
@@ -50,7 +90,7 @@ public class RecipeController {
       	
 //      	until picture getting method is made
 //      	Image pic=recipe.Picture;
-      	Image pic=new Image("gui/Noimage.jpg");
+      	Image pic=new Image("gui/Noimage.jpg", 285, 230, false, false);
       	
       	
       	//set all the values in the recipe page
@@ -59,7 +99,7 @@ public class RecipeController {
     	lRateVal.setText("Rating| Satifaction: "+Integer.toString(rate.getLiked()));
     	lBasicInfo.setText("Prep Time: "+info.getPrepTime()+"\nTotal Time:"+info.getTotalTime()+"\n\nServings: "+info.getServings()+"\nSummary: "+info.getSummary());
       	lNutriInfo.setText(recipe.getNutrition());
-    	taInstructions.setText(recipe.getInstructions()+"\n\n"+recipe.getTips());;
+    	taInstructions.setText(recipe.getInstructions()+"\n\n"+recipe.getTips());
     	taIngredients.setText(recipe.getIngredients());
     	imgPic.setImage(pic);
         
